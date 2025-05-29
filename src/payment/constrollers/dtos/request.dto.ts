3b842b0d0916079ca74bed89c0babab13558ce2a
suji6707 <heojisu6707@gmail.com>
2025-05-30 08:48:06 +0900

    added payment module and changed db schema

---
+++ src/payment/constrollers/dtos/request.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsNumber } from "class-validator";
+
+export class ChargeRequestDto {
+	@ApiProperty({ example: 10000, description: '충전 금액' })
+	@Type(() => Number)
+	@IsNumber()
+	amount: number;
+}
+
+export class PointUseRequestDto {
+	@ApiProperty({ example: 500, description: '사용 포인트' })
+	@Type(() => Number)
+	@IsNumber()
+	amount: number;
+}
---
+++ src/payment/constrollers/dtos/response.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class ChargeResponseDto {
+	@ApiProperty({ example: 10000, description: '포인트 잔액' })
+	balance: number;
+}
+
+export class BalanceResponseDto {
+	@ApiProperty({ example: 10000, description: '포인트 잔액' })
+	balance: number;
+}
+
+export class PointUseResponseDto {
+	@ApiProperty({ example: 500, description: '포인트 잔액' })
+	balance: number;
+}
+	
