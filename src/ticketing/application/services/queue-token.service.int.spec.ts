ca6c0902dd6d7dcab446e17697cb49719858ead6
suji6707 <heojisu6707@gmail.com>
2025-06-13 13:26:41 +0900

    대기열 토큰 검증 테스트(워커실행)

--- src/ticketing/application/services/queue-token.service.int.spec.ts
+++ src/ticketing/application/services/queue-token.service.int.spec.ts
@@ -6,10 +6,12 @@ import { PrismaService } from 'src/common/services/prisma.service';
 import { RedisService } from 'src/common/services/redis/redis.service';
 import { QUEUE_TOKEN_TTL, REDIS_CLIENT } from 'src/common/utils/constants';
 import { getQueueTokenKey } from 'src/common/utils/redis-keys';
+import { QueueModule } from 'src/queue/queue.module';
+import { QueueConsumer } from 'src/queue/services/queue-consumer.service';
 import { QueueProducer } from 'src/ticketing/infrastructure/external/queue-producer.service';
-import { TicketingModule } from 'src/ticketing/ticketing.module';
 import { createUser } from 'test/factories/user.factory';
 import { PrismaServiceRef } from 'test/prisma-test-setup';
+import { addJobAndStartProcess } from 'test/process/worker.mock';
 import { RedisClientRef } from 'test/redis-test-setup';
 import { TokenStatus } from '../domain/models/token';
 import { QueueTokenService } from './queue-token.service';
@@ -19,28 +21,30 @@ describe('QueueTokenService Integration Test', () => {
 	let queueTokenService: QueueTokenService;
 	let userRepository: IUserRepository;
 	let redisService: RedisService;
+	let queueProducer: QueueProducer;
+	let queueConsumer: QueueConsumer;
 
 	beforeAll(async () => {
 		app = await Test.createTestingModule({
-			imports: [CommonModule, AuthModule],
-			providers: [QueueTokenService, PrismaService, QueueProducer],
+			imports: [CommonModule, AuthModule, QueueModule],
+			providers: [
+				QueueTokenService,
+				PrismaService,
+				QueueProducer,
+				QueueConsumer,
+			],
 		})
 			.overrideProvider(PrismaService)
 			.useValue(PrismaServiceRef)
 			.overrideProvider(REDIS_CLIENT)
 			.useValue(RedisClientRef)
-			.overrideProvider(QueueProducer) // provider에 없어도 직접 override하므로 작동함
-			.useFactory({
-				factory: (redisService: RedisService): QueueProducer => {
-					return new QueueProducer(redisService);
-				},
-				inject: [RedisService],
-			})
 			.compile();
 
 		queueTokenService = app.get<QueueTokenService>(QueueTokenService);
 		userRepository = app.get<IUserRepository>('IUserRepository');
 		redisService = app.get<RedisService>(RedisService);
+		queueProducer = app.get<QueueProducer>(QueueProducer);
+		queueConsumer = app.get<QueueConsumer>(QueueConsumer);
 	});
 
 	afterAll(async () => {
@@ -48,28 +52,50 @@ describe('QueueTokenService Integration Test', () => {
 		await app.close();
 	});
 
+	let queueToken: string;
+	let userId: number;
+
 	describe('createToken', () => {
 		it('대기열 토큰을 생성한다', async () => {
 			// given
 			const user = await createUser(userRepository);
+			userId = user.id;
 
 			// when
 			const { token } = await queueTokenService.createToken({
 				userId: user.id,
 				concertId: 1,
 			});
-			console.log(token);
+			queueToken = token;
 
 			const cacheKey = getQueueTokenKey(token);
 			const status = await redisService.get(cacheKey);
-			console.log(status);
 			const ttl = await redisService.getTtl(cacheKey);
-			console.log(ttl);
 
 			// then
 			expect(status).toEqual(TokenStatus.WAITING);
 			expect(ttl).toBeGreaterThan(0);
 			expect(ttl).toBeLessThan(QUEUE_TOKEN_TTL + 1);
+		});
+	});
+
+	describe('verifyToken', () => {
+		it('대기열 토큰을 검증한다', async () => {
+			// given
+			const concertId = 1;
+			await addJobAndStartProcess(
+				queueProducer,
+				queueConsumer,
+				queueTokenService,
+				concertId,
+				queueToken,
+			);
+
+			// when
+			const result = await queueTokenService.verifyToken(userId, queueToken);
+
+			// then
+			expect(result).toBe(true);
 		});
 	});
 });
