fee35c079b11fd86542ee88a724b1cf4c1a61531
suji6707 <heojisu6707@gmail.com>
2025-06-29 11:29:57 +0900

    좌석 예약정보 변경 캐싱(hmap)

--- src/ticketing/application/services/reservation.service.ts
+++ src/ticketing/application/services/reservation.service.ts
@@ -2,12 +2,17 @@ import { TransactionHost, Transactional } from '@nestjs-cls/transactional';
 import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
 import { ConflictException, Inject, Injectable, Logger } from '@nestjs/common';
 import { IDistributedLockService } from 'src/common/interfaces/idistributed-lock.service';
+import { RedisService } from 'src/common/services/redis/redis.service';
 import {
 	DISTRIBUTED_LOCK_SERVICE,
 	SEAT_EXPIRE_TTL,
 	SEAT_LOCK_TTL,
 } from 'src/common/utils/constants';
-import { EXPIRE_QUEUE_NAME, getSeatLockKey } from 'src/common/utils/redis-keys';
+import {
+	EXPIRE_QUEUE_NAME,
+	getSeatLockKey,
+	getSeatsCacheKey,
+} from 'src/common/utils/redis-keys';
 import { PaymentService } from 'src/payment/application/services/payment.service';
 import { QueueProducer } from 'src/ticketing/infrastructure/external/queue-producer.service';
 import {
@@ -38,6 +43,7 @@ export class ReservationService {
 		private readonly queueProducer: QueueProducer,
 		@Inject(DISTRIBUTED_LOCK_SERVICE)
 		private readonly distributedLockService: IDistributedLockService,
+		private readonly redisService: RedisService,
 	) {}
 
 	async temporaryReserve(
@@ -45,64 +51,84 @@ export class ReservationService {
 		seatId: number,
 		queueToken: string,
 	): Promise<ReserveResponseDto> {
-		// token verify
-		const isValidToken = await this.queueTokenService.verifyToken(
-			userId,
-			queueToken,
-		);
-		if (!isValidToken) {
-			throw new Error('Invalid queue token');
-		}
-
-		// domain logic
-		const seat = await this.seatRepository.findOne(seatId);
-		seat.setReserved();
-		const reservation = new Reservation({
-			userId,
-			seatId: seat.id,
-			purchasePrice: seat.price,
-		});
-
-		// 분산락 + db 조건부 UPDATE
-		let newReservation: Reservation;
-		let paymentToken: string;
 		try {
-			newReservation = await this.distributedLockService.withLock<Reservation>(
-				getSeatLockKey(seatId),
-				SEAT_LOCK_TTL,
-				async () => {
-					return await this._reserveWithOptimisticLock(seat, reservation);
-				},
-				1, // 재시도 X. 한 요청만 락을 획득하고 나머지는 실패하는것이 정상
-			);
-			// 결제토큰 발급
-			const { token } = await this.paymentTokenService.createToken({
+			// token verify
+			const isValidToken = await this.queueTokenService.verifyToken(
 				userId,
-				seatId,
+				queueToken,
+			);
+			if (!isValidToken) {
+				throw new Error('Invalid queue token');
+			}
+
+			// domain logic
+			const seat = await this.seatRepository.findOne(seatId);
+			seat.setReserved();
+			const reservation = new Reservation({
+				userId,
+				seatId: seat.id,
+				purchasePrice: seat.price,
 			});
-			paymentToken = token;
+
+			// 분산락 + db 조건부 UPDATE
+			let newReservation: Reservation;
+			let paymentToken: string;
+			try {
+				newReservation =
+					await this.distributedLockService.withLock<Reservation>(
+						getSeatLockKey(seatId),
+						SEAT_LOCK_TTL,
+						async () => {
+							// BEGIN
+							return await this._reserveWithOptimisticLock(seat, reservation);
+							// COMMIT
+						},
+						1, // 재시도 X. 한 요청만 락을 획득하고 나머지는 실패하는것이 정상
+					);
+
+				// Write back to cache
+				const cacheKey = getSeatsCacheKey(seatId);
+				const obj = {
+					[seatId]: {
+						className: seat.className,
+						price: seat.price,
+						status: seat.status,
+					},
+				};
+				await this.redisService.hsetField(cacheKey, obj);
+
+				// 결제토큰 발급
+				const { token } = await this.paymentTokenService.createToken({
+					userId,
+					seatId,
+				});
+				paymentToken = token;
+			} catch (error) {
+				this.logger.error(error);
+				throw new Error('FAILED_TO_ACQUIRE_LOCK'); // 이미 예약된 좌석입니다
+			}
+
+			// 5분뒤 만료 작업을 큐에 추가
+			await this.queueProducer.addJob(
+				EXPIRE_QUEUE_NAME,
+				{
+					reservationId: newReservation.id,
+					seatId: seatId,
+					lockToken: queueToken, // 잠금 해제시 필요
+				},
+				{
+					delay: SEAT_EXPIRE_TTL * 1000, // 5분 지연!!
+				},
+			);
+
+			return {
+				reservationId: newReservation.id,
+				paymentToken,
+			};
 		} catch (error) {
 			this.logger.error(error);
-			throw new Error('FAILED_TO_ACQUIRE_LOCK'); // 이미 예약된 좌석입니다
+			throw error;
 		}
-
-		// 5분뒤 만료 작업을 큐에 추가
-		await this.queueProducer.addJob(
-			EXPIRE_QUEUE_NAME,
-			{
-				reservationId: newReservation.id,
-				seatId: seatId,
-				lockToken: queueToken, // 잠금 해제시 필요
-			},
-			{
-				delay: SEAT_EXPIRE_TTL * 1000, // 5분 지연!!
-			},
-		);
-
-		return {
-			reservationId: newReservation.id,
-			paymentToken,
-		};
 	}
 
 	@Transactional()
@@ -187,5 +213,9 @@ export class ReservationService {
 				paidAt: updatedReservation.paidAt,
 			},
 		};
+	}
+
+	async getInfo(reservationId: number): Promise<optional<Reservation>> {
+		return this.reservationRepository.findOne(reservationId);
 	}
 }
